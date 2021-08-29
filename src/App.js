1386cd8025a67d0c5528c23bc412a5dae4a24dd2
SheezChill <rageendergaming@gmail.com>
2021-08-29 09:51:22 +0700

    cleaned code

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App() {
   const [populated, setPopulated] = useState(true);
   const [currentHovered, setCurrentHovered] = useState(false);
   const [rawName, setRawName] = useState('keqing');
-  const [name, setName] = useState(`<strong>${rawName}</strong>`)
+  const [name, setName] = useState(`<strong>${rawName}</strong>`);
   const [nameCaps, setNameCaps] = useState(rawName.toUpperCase());
   const [currentActive, setCurrentActive] = useState('wangy');
-  const [rawTemplate, setRawTemplate] = useState('${nameCaps} ${nameCaps} ${nameCaps} ❤️ ❤️ ❤️ WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya rambut ${name} wangi aku mau nyiumin aroma wanginya ${name} AAAAAAAAH ~ Rambutnya.... aaah rambutnya juga pengen aku elus-elus ~~~~ AAAAAH ${name} keluar pertama kali di anime juga manis ❤️ ❤️ ❤️ banget AAAAAAAAH ${nameCaps} AAAAA LUCCUUUUUUUUUUUUUUU............${nameCaps} AAAAAAAAAAAAAAAAAAAAGH ❤️ ❤️ ❤️apa ? ${name} itu gak nyata ? Cuma HALU katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK PEDULI BANGSAAAAAT !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. ❤️ ❤️ ❤️ ${name} gw ...${name} di laptop ngeliatin gw, ${name} .. kamu percaya sama aku ? aaaaaaaaaaah syukur ${name} aku gak mau merelakan ${name} aaaaaah ❤️ ❤️ ❤️ YEAAAAAAAAAAAH GUA MASIH PUNYA ${nameCaps} SENDIRI PUN NGGAK SAMA AAAAAAAAAAAAAAH')
+  const [rawTemplate, setRawTemplate] = useState(Templates[0].copypasta);
   const [currentTemplate, setCurrentTemplate] = useState(eval('`' + rawTemplate + '`'));
   const hoveredBefore = useRef('');
 
   const populatedListener = (e) => {
     if (e.target.value) {
-      setPopulated(true)
+      setPopulated(true);
     } else {
-      setPopulated(false)
+      setPopulated(false);
     }
   }
 
@@ -25,10 +25,10 @@ function App() {
     hoveredBefore.current = e.relatedTarget;
     setCurrentHovered(e.target);
   }
-
+  
   useEffect(() => {
     setCurrentTemplate(eval('`' + rawTemplate + '`'));
-  }, [rawTemplate])
+  }, [rawTemplate, nameCaps])
 
   useEffect(() => {
     setName(`<strong>${rawName}</strong>`);
@@ -38,14 +38,18 @@ function App() {
     setNameCaps(name.toUpperCase());
   }, [name])
 
-  useEffect(() => {
-    setCurrentTemplate(eval('`' + rawTemplate + '`'));
-  }, [nameCaps])
-
   return (
     <div className="App">
       <div className={populated ? 'wrap populated' : 'wrap'}>
-        <input type="text" className="input" maxLength="20" spellCheck="false" value={rawName} onKeyUp={e => populatedListener(e)} onChange={e => setRawName(e.target.value.toLowerCase())} />
+        <input
+          type="text"
+          className="input"
+          maxLength="20"
+          spellCheck="false"
+          value={rawName}
+          onKeyUp={e => populatedListener(e)}
+          onChange={e => setRawName(e.target.value.toLowerCase())}
+        />
         <label>Name</label>
       </div>
       <div className="templates-container">
@@ -61,12 +65,22 @@ function App() {
           ></div>
           {
             Templates.map((template, index) => {
-              return <button className={currentActive === template.templateName ? 'active': null} key={index} onMouseEnter={e => moveButtonHighlight(e)} onClick={() => {setRawTemplate(template.copypasta); setCurrentActive(template.templateName)}}>{template.templateName}</button>
+              return (
+                <button
+                  key={index}
+                  className={currentActive === template.templateName ? 'active': null}
+                  onMouseEnter={e => moveButtonHighlight(e)}
+                  onClick={() => {setRawTemplate(template.copypasta); setCurrentActive(template.templateName)}}
+                >{template.templateName}</button>
+              )
             })
           }
         </div>
       </div>
-    <div className="output" dangerouslySetInnerHTML={{ __html: rawName ? rawName === "sheez" ? 'No.' : currentTemplate : 'Input name' }}></div>
+    <div
+      className="output"
+      dangerouslySetInnerHTML={{ __html: rawName ? rawName === "sheez" ? 'No.' : currentTemplate : 'Input name' }}
+    ></div>
     </div>
   );
 }
